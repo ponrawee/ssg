3401ef06a7ba113c3591f9cd263e416f1530a710
heytitle <pat.chormai@gmail.com>
2019-07-15 13:51:32 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+"""
+Thai syllable segmentation using Conditional Random Field
+"""
+
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='ssg',
+    packages=['ssg'],
+    scripts=["scripts/ssg-cli"],
+    include_package_data=True,
+    version='0.0.1',
+    install_requires=requirements,
+    license='MIT',
+    description='Thai syllable segmentation using Conditional Random Field'
+    author="Ponrawee Prasertsom",
+    author_email='ponrawee.pra@gmail.com",
+    url="https://github.com/ponrawee/ssg",
+    download_url="https://github.com/ponrawee/ssg.git",
+    keywords=["crf", "syllable", "tokenization", "thai"],
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ],
+)
