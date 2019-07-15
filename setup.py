79fe06a652182ab82b6a88411cb453b89cab413e
heytitle <pat.chormai@gmail.com>
2019-07-15 13:52:47 +0700

    add ./artifacts to setup.py

--- setup.py
+++ setup.py
@@ -6,24 +6,26 @@ Thai syllable segmentation using Conditional Random Field
 
 from setuptools import setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='ssg',
-    packages=['ssg'],
+    name="ssg",
+    packages=["ssg"],
+    package_dir={"ssg": "ssg"},
+    package_data={"ssg": ["artifacts/*"]},
     scripts=["scripts/ssg-cli"],
     include_package_data=True,
-    version='0.0.1',
+    version="0.0.1",
     install_requires=requirements,
-    license='MIT',
-    description='Thai syllable segmentation using Conditional Random Field'
+    license="MIT",
+    description="Thai syllable segmentation using Conditional Random Field",
     author="Ponrawee Prasertsom",
-    author_email='ponrawee.pra@gmail.com",
+    author_email="ponrawee.pra@gmail.com",
     url="https://github.com/ponrawee/ssg",
     download_url="https://github.com/ponrawee/ssg.git",
     keywords=["crf", "syllable", "tokenization", "thai"],
     classifiers=[
-        'Development Status :: 3 - Alpha'
+        "Development Status :: 3 - Alpha"
     ],
 )
